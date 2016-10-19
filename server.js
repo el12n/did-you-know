1645f80ad037db24025c77a4aa75d72598a214c1
Alvaro Lockhart <alvarolock@gmail.com>
2016-10-19 19:02:28 +0000

    Fixing to launch with heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname+"/public/views"));
 app.use('/views',express.static(__dirname+"/public/views"));
 
@@ -10,6 +14,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('App listening on port 8080!');
 });
